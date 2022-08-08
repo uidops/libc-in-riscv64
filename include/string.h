27c71feabe5851ed384a3a6402f54b07c16f298a
uidops <uidops@protonmail.com>
2022-08-08 19:46:05 +0430

    code style correction in string.h

--- include/string.h
+++ include/string.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 
 size_t strlen(const char *);
-char * strcpy(char *, const char *);
+char *strcpy(char *, const char *);
 
 void *memset(void *, int, size_t);
 void *memcpy(void *, const void *, size_t);
