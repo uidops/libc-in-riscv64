b20ab287ca5aa4b13c342e0c19b74f1720d18ac4
uidops <uidops@protonmail.com>
2022-08-08 20:32:49 +0430

    added strcmp prototype in string.h

--- include/string.h
+++ include/string.h
@@ -5,6 +5,7 @@
 
 size_t strlen(const char *);
 char *strcpy(char *, const char *);
+int strcmp(const char *, const char *);
 
 void *memset(void *, int, size_t);
 void *memcpy(void *, const void *, size_t);
