2390e3da7a7b4c1a78fb7828f884e5b1c965faa6
uidops <uidops@protonmail.com>
2022-08-08 18:36:30 +0430

    updated memset function

--- string/memset.s
+++ string/memset.s
@@ -6,11 +6,11 @@
 memset:
 	and t0, a0, a2
 	beq t0, x0, .end
-	add s0, x0, a0
+	add s1, x0, a0
 	.loop:
-		sb  a1, (s0)
+		sb  a1, (s1)
 	    addi a2, a2, -1
-		addi s0, s0, 1
+		addi s1, s1, 1
 		bne a2, x0, .loop
 	.end:
 		ret
