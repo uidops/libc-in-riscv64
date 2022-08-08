c2181a830f7b45852182d3ac6a0c38787913600d
uidops <uidops@protonmail.com>
2022-08-08 17:50:12 +0430

    added memcpy's test

---
+++ string/test/memcpy.c
@@ -0,0 +1,18 @@
+#include "string.h"
+
+int
+main(void)
+{
+	int a[5] = {1, 2, 3, 4, 5};	
+	int b[5] = {0, 0, 0, 0, 0};	
+
+	memcpy(b, a, sizeof(int)*4);
+
+	if ((b[0] == 1 && b[1] == 2) &&
+			(b[2] == 3 && b[3] == 4) &&
+			(b[4] == 0))
+		return 0;
+
+	return 1;
+}
+
