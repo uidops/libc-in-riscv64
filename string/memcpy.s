d3fec6257832647c457852e161466788c26d97d6
uidops <uidops@protonmail.com>
2022-08-08 18:32:06 +0430

    fixed segmentation fault in memcpy function

--- string/memcpy.s
+++ string/memcpy.s
@@ -3,14 +3,14 @@
 
 .type memcpy, @function
 
-memset:
+memcpy:
 	beq a2, x0, .end
-	add s0, x0, a0
+	add s1, x0, a0
 	.loop:
 		lb t0, (a1)
-		sb t0, (s0)
-	    addi a2, a2, -1
-		addi s0, s0, 1
+		sb t0, (s1)
+		addi a2, a2, -1
+		addi s1, s1, 1
 		addi a1, a1, 1
 		bne a2, x0, .loop
 	.end:
